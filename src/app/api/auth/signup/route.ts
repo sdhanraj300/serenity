984c4e1990e5101e8536ad79ad6685ab3f9457e7
Dhanraj Singh <sdhanraj300@gmail.com>
2024-10-10 16:28:07 +0530

    7

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -2,34 +2,47 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import bcrypt from "bcryptjs";
+
 export async function POST(req: NextRequest) {
-  const body = await req.json();
-  const { email, password, name } = body;
-  if (!email || !password || !name) {
-    return NextResponse.json(
-      { error: "Please enter all the details" },
-      {
-        status: 400,
-      }
-    );
-  }
-  const user = await prisma.user.findFirst({
-    where: {
-      email,
-    },
-  });
-  if (user) {
-    return NextResponse.json({ error: "User already exists" });
-  }
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = await prisma.user.create({
-    data: {
-      email,
-      password: hashedPassword,
-      name,
-    },
-  });
-  if (newUser) {
+  try {
+    const body = await req.json();
+    const { email, password, name } = body;
+
+    // Validate input
+    if (!email || !password || !name) {
+      return NextResponse.json(
+        { error: "Please enter all the details" },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    // Check if user already exists
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 409 } // Conflict
+      );
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+      },
+    });
+
     return NextResponse.json(
       {
         success: "User created successfully",
@@ -39,9 +52,10 @@ export async function POST(req: NextRequest) {
         status: 201,
       }
     );
-  } else {
+  } catch (error) {
+    console.error("Error creating user:", error);
     return NextResponse.json(
-      { error: "User creation failed" },
+      { error: "Internal Server Error" },
       {
         status: 500,
       }
